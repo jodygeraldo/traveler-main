e20b2f1dd77126f34cf24e213f56ec2a04826156
Jody Geraldo <ajody.geraldo@gmail.com>
2022-06-29 19:23:46 +0700

    character page as default page

--- app/routes/__app.tsx
+++ app/routes/__app.tsx
@@ -23,7 +23,7 @@ const user = {
     'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
 }
 const navigation = [
-  { name: 'Character', to: '/' },
+  { name: 'Character', to: '/character' },
   { name: 'Inventory', to: '/inventory' },
 ]
 const userNavigation = [
--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -62,7 +62,7 @@ export const meta: MetaFunction = () => {
 
 export default function LoginPage() {
   const [searchParams] = useSearchParams()
-  const redirectTo = searchParams.get('redirectTo') || '/inventory'
+  const redirectTo = searchParams.get('redirectTo') || '/character'
   const actionData = useActionData() as ActionData
   const emailRef = React.useRef<HTMLInputElement>(null)
   const passwordRef = React.useRef<HTMLInputElement>(null)
