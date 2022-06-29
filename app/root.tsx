6a7fb53964722858fa295f546f18092303476c5e
Jody Geraldo <ajody.geraldo@gmail.com>
2022-06-29 15:38:02 +0700

    revert: get user data on root loader

--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,7 @@
-import type { LinksFunction, MetaFunction } from '@remix-run/node'
+import type { LinksFunction, LoaderFunction, MetaFunction } from '@remix-run/node'
+import { json } from '@remix-run/node'
 import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from '@remix-run/react'
+import { getUser } from './session.server'
 
 import tailwindStylesheetUrl from './tailwind.css'
 
@@ -12,6 +14,16 @@ export const meta: MetaFunction = () => ({
   title: 'Traveler Main',
   viewport: 'width=device-width,initial-scale=1',
 })
+
+type LoaderData = {
+  user: Awaited<ReturnType<typeof getUser>>
+}
+
+export const loader: LoaderFunction = async ({ request }) => {
+  return json<LoaderData>({
+    user: await getUser(request),
+  })
+}
 
 export default function App() {
   return (
