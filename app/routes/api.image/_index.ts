714398ec9d6c236d1cbbbbec292e49dc39bee78c
Jody Geraldo <ajody.geraldo@gmail.com>
2022-07-01 12:20:39 +0700

    set fallback image to png

--- app/routes/api.image/_index.ts
+++ app/routes/api.image/_index.ts
@@ -1,11 +1,14 @@
 import type { LoaderFunction } from '@remix-run/node'
 import { sharpTransformer } from 'remix-image-sharp'
+import type { LoaderConfig } from 'remix-image/server'
+import { MimeType } from 'remix-image/server'
 import { DiskCache, imageLoader } from 'remix-image/server'
 
-const config = {
+const config: LoaderConfig = {
   selfUrl: 'http://localhost:3000',
   cache: new DiskCache(),
   transformer: sharpTransformer,
+  fallbackFormat: MimeType.PNG,
 }
 
 export const loader: LoaderFunction = ({ request }) => {
