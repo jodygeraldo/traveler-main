a07f2c7d9f647e520c2d1be3034405c43203ca52
Jody Geraldo <ajody.geraldo@gmail.com>
2022-07-02 15:49:49 +0700

    accept `defaultValues` and `errors`

--- app/components/ManualLevelForm.tsx
+++ app/components/ManualLevelForm.tsx
@@ -3,10 +3,18 @@ import type { InputPropType } from 'remix-params-helper'
 import { Button } from './Button'
 
 type Props = {
+  defaultValues: {
+    level: number
+    ascension: number
+    normalAttack: number
+    elementalSkill: number
+    elementalBurst: number
+  }
   inputProps: (key: string, options?: any) => InputPropType
+  errors?: { [key: string]: string }
 }
 
-export default function ManualLevelForm({ inputProps }: Props) {
+export default function ManualLevelForm({ defaultValues, inputProps, errors }: Props) {
   return (
     <div className="mt-8">
       <Form method="post">
@@ -18,8 +26,14 @@ export default function ManualLevelForm({ inputProps }: Props) {
             <input
               id="level"
               className="mt-1 block w-full rounded-md border-gray-7 bg-gray-3 shadow-sm focus:border-primary-8 focus:ring-primary-8 sm:text-sm"
+              defaultValue={defaultValues.level}
               {...inputProps('level')}
+              aria-invalid={!!errors?.level}
+              aria-describedby="level-error"
             />
+            <p className="mt-2 text-sm text-danger-9" id="level-error">
+              {errors?.level}
+            </p>
           </div>
 
           <div className="col-span-6 sm:col-span-3">
@@ -29,9 +43,15 @@ export default function ManualLevelForm({ inputProps }: Props) {
             <input
               id="ascension"
               className="mt-1 block w-full rounded-md border-gray-7 bg-gray-3 shadow-sm focus:border-primary-8 focus:ring-primary-8 sm:text-sm"
+              defaultValue={defaultValues.ascension}
               min={0} // the params helper not correctly read the min value (0)
               {...inputProps('ascension')}
+              aria-invalid={!!errors?.ascension}
+              aria-describedby="ascension-error"
             />
+            <p className="mt-2 text-sm text-danger-9" id="ascension-error">
+              {errors?.ascension}
+            </p>
           </div>
 
           <div className="col-span-6 sm:col-span-2">
@@ -41,8 +61,14 @@ export default function ManualLevelForm({ inputProps }: Props) {
             <input
               id="normal-attack"
               className="mt-1 block w-full rounded-md border-gray-7 bg-gray-3 shadow-sm focus:border-primary-8 focus:ring-primary-8 sm:text-sm"
+              defaultValue={defaultValues.normalAttack}
               {...inputProps('normalAttack')}
+              aria-invalid={!!errors?.normalAttack}
+              aria-describedby="normal-attack-error"
             />
+            <p className="mt-2 text-sm text-danger-9" id="normal-attack-error">
+              {errors?.normalAttack}
+            </p>
           </div>
 
           <div className="col-span-6 sm:col-span-2">
@@ -52,8 +78,14 @@ export default function ManualLevelForm({ inputProps }: Props) {
             <input
               id="elemental-skill"
               className="mt-1 block w-full rounded-md border-gray-7 bg-gray-3 shadow-sm focus:border-primary-8 focus:ring-primary-8 sm:text-sm"
+              defaultValue={defaultValues.elementalSkill}
               {...inputProps('elementalSkill')}
+              aria-invalid={!!errors?.elementalSkill}
+              aria-describedby="elemental-skill-error"
             />
+            <p className="mt-2 text-sm text-danger-9" id="elemental-skill-error">
+              {errors?.elementalSkill}
+            </p>
           </div>
 
           <div className="col-span-6 sm:col-span-2">
@@ -63,8 +95,14 @@ export default function ManualLevelForm({ inputProps }: Props) {
             <input
               id="elemental-burst"
               className="mt-1 block w-full rounded-md border-gray-7 bg-gray-3 shadow-sm focus:border-primary-8 focus:ring-primary-8 sm:text-sm"
+              defaultValue={defaultValues.elementalBurst}
               {...inputProps('elementalBurst')}
+              aria-invalid={!!errors?.elementalBurst}
+              aria-describedby="elemental-burst-error"
             />
+            <p className="mt-2 text-sm text-danger-9" id="elemental-burst-error">
+              {errors?.elementalBurst}
+            </p>
           </div>
         </div>
         <div className="mt-8 text-right">
