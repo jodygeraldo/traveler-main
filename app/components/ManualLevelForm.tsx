5c2d1c60f18b3f6567ba74ff3c02bd3bc062b14b
Jody Geraldo <ajody.geraldo@gmail.com>
2022-07-02 18:42:53 +0700

    add modal alert on save button

--- app/components/ManualLevelForm.tsx
+++ app/components/ManualLevelForm.tsx
@@ -1,7 +1,12 @@
 import { Form } from '@remix-run/react'
 import type { InputPropType } from 'remix-params-helper'
+import { useHydrated } from 'remix-utils'
 import type { TravelerData } from '~/routes/_app.character.traveler.$vision.manual-levelup/_index'
 import { Button } from './Button'
+
+import { Fragment, useRef, useState } from 'react'
+import { Dialog, Transition } from '@headlessui/react'
+import Icon from './Icon'
 
 type Props = {
   defaultValues: {
@@ -22,9 +27,19 @@ export default function ManualLevelForm({
   errors,
   hiddenTravelersData,
 }: Props) {
+  const hydrated = useHydrated()
+  const alertState = useState(false)
+  const ref = useRef<HTMLFormElement>(null)
+
+  function handleClick() {
+    // const formData = new FormData(ref.current ?? undefined)
+
+    alertState[1](true)
+  }
+
   return (
     <div className="mt-8">
-      <Form method="post">
+      <Form ref={ref} method="post">
         <div className="grid grid-cols-6 gap-6">
           <div className="col-span-6 sm:col-span-3">
             <label htmlFor="level" className="block text-sm font-medium text-gray-12">
@@ -114,11 +129,106 @@ export default function ManualLevelForm({
         </div>
         <input type="hidden" name="travelersData" value={JSON.stringify(hiddenTravelersData)} />
         <div className="mt-8 text-right">
-          <Button type="submit" focusRing={1}>
+          <Button
+            type={hydrated ? 'button' : 'submit'}
+            onClick={hydrated ? handleClick : undefined}
+            focusRing={1}
+          >
             Save
           </Button>
         </div>
       </Form>
+      <Alert state={alertState} />
     </div>
+  )
+}
+
+function Alert({ state }: { state: [boolean, React.Dispatch<React.SetStateAction<boolean>>] }) {
+  const [open, setOpen] = state
+
+  const cancelButtonRef = useRef(null)
+
+  return (
+    <Transition.Root show={open} as={Fragment}>
+      <Dialog as="div" className="relative z-10" initialFocus={cancelButtonRef} onClose={setOpen}>
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 bg-overlay-black-9 transition-opacity" />
+        </Transition.Child>
+
+        <div className="fixed inset-0 z-10 overflow-y-auto">
+          <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              enterTo="opacity-100 translate-y-0 sm:scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            >
+              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-gray-2 px-4 pt-5 pb-4 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6">
+                <div className="sm:flex sm:items-start">
+                  <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-info-1 sm:mx-0 sm:h-10 sm:w-10">
+                    <Icon
+                      type="outline"
+                      name="informationCircle"
+                      className="h-6 w-6 text-info-9"
+                      aria-hidden="true"
+                    />
+                  </div>
+                  <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                    <Dialog.Title as="h3" className="text-lg font-medium leading-6 text-gray-12">
+                      Manual level up
+                    </Dialog.Title>
+                    <div className="mt-2 text-sm text-gray-11">
+                      <p>Manual level up generally not recommended.</p>
+                      {/* <p>You changed:</p>
+                      <ul>
+                        <li>
+                          Level from <span className="font-bold">1</span>
+                          <span className="sr-only">To</span>
+                          <Icon
+                            type="solid"
+                            name="arrowSmRight"
+                            aria-hidden
+                            className="inline h-4 w-4"
+                          />
+                          <span className="font-bold">2</span>
+                        </li>
+                      </ul> */}
+                    </div>
+                  </div>
+                </div>
+                <div className="mt-5 sm:mt-4 sm:flex sm:flex-row-reverse">
+                  <Button
+                    className="w-full sm:ml-3 sm:w-auto"
+                    variant="info"
+                    onClick={() => setOpen(false)}
+                  >
+                    Confirm changes
+                  </Button>
+                  <Button
+                    className="mt-3 w-full sm:mt-0 sm:ml-3 sm:w-auto"
+                    variant="basic"
+                    onClick={() => setOpen(false)}
+                    ref={cancelButtonRef}
+                  >
+                    Cancel
+                  </Button>
+                </div>
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
+        </div>
+      </Dialog>
+    </Transition.Root>
   )
 }
--- app/icons/outline.ts
+++ app/icons/outline.ts
@@ -5,5 +5,6 @@ import filter from './outline/filter.svg'
 import menu from './outline/menu.svg'
 import search from './outline/search.svg'
 import x from './outline/x.svg'
+import informationCircle from './outline/information-circle.svg'
 
-export default { menu, search, bookmark, bookOpen, filter, dotsVertical, x }
+export default { menu, search, bookmark, bookOpen, filter, dotsVertical, x, informationCircle }
