73deff3d4017e15d0e4088aeb0ae24eb841217f7
Jody Geraldo <ajody.geraldo@gmail.com>
2022-07-04 15:01:30 +0700

    refactor: testing (#15)

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,19 +2,6 @@
  * @type {import('@types/eslint').Linter.BaseConfig}
  */
 module.exports = {
-  ignorePatterns: ['**/*.d.ts'],
-  extends: [
-    '@remix-run/eslint-config',
-    '@remix-run/eslint-config/node',
-    '@remix-run/eslint-config/jest-testing-library',
-    'prettier',
-  ],
-  // we're using vitest which has a very similar API to jest
-  // (so the linting plugins work nicely), but it means we have to explicitly
-  // set the jest version.
-  settings: {
-    jest: {
-      version: 27,
-    },
-  },
+  ignorePatterns: ['**/*.d.ts', "coverage"],
+  extends: ['@remix-run/eslint-config', '@remix-run/eslint-config/node', 'prettier'],
 }
