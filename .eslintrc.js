39858a8babdc3b53266b34ee6fb21e0e5a739c7a
Jody Geraldo <ajody.geraldo@gmail.com>
2022-07-04 14:38:33 +0700

    update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,6 @@
  * @type {import('@types/eslint').Linter.BaseConfig}
  */
 module.exports = {
-  ignorePatterns: ['**/*.d.ts'],
+  ignorePatterns: ['**/*.d.ts', "coverage"],
   extends: ['@remix-run/eslint-config', '@remix-run/eslint-config/node', 'prettier'],
 }
