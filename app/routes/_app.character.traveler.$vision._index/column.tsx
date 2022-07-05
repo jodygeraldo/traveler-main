afff139a44250c48a974b6d8dc5d49e6e38effde
Jody Geraldo <ajody.geraldo@gmail.com>
2022-07-05 15:08:32 +0700

    chore: format

--- app/routes/_app.character.traveler.$vision._index/column.tsx
+++ app/routes/_app.character.traveler.$vision._index/column.tsx
@@ -1,6 +1,6 @@
-import type * as ReactTable from '@tanstack/react-table';
-import * as Icon from '~/components/Icon';
-import TableCell from '~/components/TableCell';
+import type * as ReactTable from '@tanstack/react-table'
+import * as Icon from '~/components/Icon'
+import TableCell from '~/components/TableCell'
 
 interface Ascension {
   phase: { from: number; to: number }
@@ -61,49 +61,49 @@ interface Talent {
 }
 
 export const talent: ReactTable.ColumnDef<Talent>[] = [
-    {
-      header: 'Level',
-      accessorKey: 'level',
-      cell: ({ getValue }) => (
-        <div className="flex items-center gap-1">
-          <span className="tabular-nums">{getValue().from}</span>
-          <span className="sr-only">To</span>
-          <Icon.Solid name="arrowSmRight" aria-hidden className="h-4 w-4" />
-          <span className="tabular-nums">{getValue().to}</span>
-        </div>
-      ),
-    },
-    {
-      header: 'Mora',
-      accessorKey: 'mora',
-      cell: ({ getValue }) => <TableCell quantity={getValue()} text="Mora" />,
-    },
-    {
-      header: 'Common',
-      accessorKey: 'common',
-      cell: ({ getValue }) => (
-        <TableCell quantity={getValue().quantity} text={getValue().name} />
-      ),
-    },
-    {
-      header: 'Book',
-      accessorKey: 'book',
-      cell: ({ getValue }) => (
-        <TableCell quantity={getValue().quantity} text={getValue().name} />
-      ),
-    },
-    {
-      header: 'Boss',
-      accessorKey: 'boss',
-      cell: ({ getValue }) => (
-        <TableCell quantity={getValue()?.quantity} text={getValue()?.name} />
-      ),
-    },
-    {
-      header: 'Special',
-      accessorKey: 'special',
-      cell: ({ getValue }) => (
-        <TableCell quantity={getValue()?.quantity} text={getValue()?.name} />
-      ),
-    },
-  ]
+  {
+    header: 'Level',
+    accessorKey: 'level',
+    cell: ({ getValue }) => (
+      <div className="flex items-center gap-1">
+        <span className="tabular-nums">{getValue().from}</span>
+        <span className="sr-only">To</span>
+        <Icon.Solid name="arrowSmRight" aria-hidden className="h-4 w-4" />
+        <span className="tabular-nums">{getValue().to}</span>
+      </div>
+    ),
+  },
+  {
+    header: 'Mora',
+    accessorKey: 'mora',
+    cell: ({ getValue }) => <TableCell quantity={getValue()} text="Mora" />,
+  },
+  {
+    header: 'Common',
+    accessorKey: 'common',
+    cell: ({ getValue }) => (
+      <TableCell quantity={getValue().quantity} text={getValue().name} />
+    ),
+  },
+  {
+    header: 'Book',
+    accessorKey: 'book',
+    cell: ({ getValue }) => (
+      <TableCell quantity={getValue().quantity} text={getValue().name} />
+    ),
+  },
+  {
+    header: 'Boss',
+    accessorKey: 'boss',
+    cell: ({ getValue }) => (
+      <TableCell quantity={getValue()?.quantity} text={getValue()?.name} />
+    ),
+  },
+  {
+    header: 'Special',
+    accessorKey: 'special',
+    cell: ({ getValue }) => (
+      <TableCell quantity={getValue()?.quantity} text={getValue()?.name} />
+    ),
+  },
+]
