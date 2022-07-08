0be37463ddbbd438f4993adf7bb86e2278bd15ef
Jody Geraldo <jody.geraldo.jg@gmail.com>
2022-07-08 14:18:57 +0700

    chore: refresh edgeql-js

--- dbschema/edgeql-js/syntax/detached.ts
+++ dbschema/edgeql-js/syntax/detached.ts
@@ -1,5 +1,5 @@
-import { Expression, ExpressionKind, TypeSet } from 'edgedb/dist/reflection'
-import { $expressionify } from './path'
+import {Expression, ExpressionKind, TypeSet} from "edgedb/dist/reflection";
+import {$expressionify} from "./path";
 
 export function detached<Expr extends TypeSet>(
   expr: Expr
@@ -9,12 +9,12 @@ export function detached<Expr extends TypeSet>(
     __cardinality__: expr.__cardinality__,
     __expr__: expr,
     __kind__: ExpressionKind.Detached,
-  }) as any
+  }) as any;
 }
 
 export type $expr_Detached<Expr extends TypeSet = TypeSet> = Expression<{
-  __element__: Expr['__element__']
-  __cardinality__: Expr['__cardinality__']
-  __kind__: ExpressionKind.Detached
-  __expr__: Expr
-}>
+  __element__: Expr["__element__"];
+  __cardinality__: Expr["__cardinality__"];
+  __kind__: ExpressionKind.Detached;
+  __expr__: Expr;
+}>;
