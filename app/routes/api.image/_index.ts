d816a569fe0b3723b8d4cc0b29cc42800ecd0514
Jody Geraldo <ajody.geraldo@gmail.com>
2022-07-11 00:42:08 +0700

    deployment (#26) (#27)
    * update fly and docker config
    
    * change app name
    
    * update actions
    
    * update package.json
    
    * update fly config
    
    * fix: image path

--- app/routes/api.image/_index.ts
+++ app/routes/api.image/_index.ts
@@ -2,16 +2,20 @@ import type * as RemixNode from '@remix-run/node'
 import * as RemixImageSharp from 'remix-image-sharp'
 import * as RemixImageServer from 'remix-image/server'
 
-const config: RemixImageServer.LoaderConfig = {
-  selfUrl: 'http://localhost:3000',
-  cache: new RemixImageServer.DiskCache(),
-  transformer: RemixImageSharp.sharpTransformer,
-  fallbackFormat: RemixImageServer.MimeType.PNG,
-  defaultOptions: {
-    contentType: RemixImageServer.MimeType.WEBP,
-  },
-}
 
 export const loader: RemixNode.LoaderFunction = ({ request }) => {
+  const host =
+    request.headers.get('X-Forwarded-Host') ?? request.headers.get('host')
+  
+  const config: RemixImageServer.LoaderConfig = {
+    selfUrl: `http://${host}`,
+    cache: new RemixImageServer.DiskCache(),
+    transformer: RemixImageSharp.sharpTransformer,
+    fallbackFormat: RemixImageServer.MimeType.PNG,
+    defaultOptions: {
+      contentType: RemixImageServer.MimeType.WEBP,
+    },
+  }
+  
   return RemixImageServer.imageLoader(config, request)
 }
--- app/routes/healthcheck/_index.tsx
+++ app/routes/healthcheck/_index.tsx
@@ -1,4 +1,3 @@
-// learn more: https://fly.io/docs/reference/configuration/#services-http_checks
 import type * as RemixNode from '@remix-run/node'
 import prisma from '~/db.server'
 
@@ -8,8 +7,6 @@ export const loader: RemixNode.LoaderFunction = async ({ request }) => {
 
   try {
     const url = new URL('/', `http://${host}`)
-    // if we can connect to the database and make a simple query
-    // and make a HEAD request to ourselves, then we're good.
     await Promise.all([
       prisma.user.count(),
       fetch(url.toString(), { method: 'HEAD' }).then((r) => {
