6c82ddf204ab99213cc71674fcaf24cc20eb2d71
Jody Geraldo <ajody.geraldo@gmail.com>
2022-07-11 00:31:47 +0700

    revert: old healthcheck

--- app/routes/healthcheck/_index.tsx
+++ app/routes/healthcheck/_index.tsx
@@ -1,9 +1,18 @@
 import type * as RemixNode from '@remix-run/node'
 import prisma from '~/db.server'
 
-export const loader: RemixNode.LoaderFunction = async () => {
+export const loader: RemixNode.LoaderFunction = async ({ request }) => {
+  const host =
+    request.headers.get('X-Forwarded-Host') ?? request.headers.get('host')
+
   try {
-    await prisma.user.count()
+    const url = new URL('/', `http://${host}`)
+    await Promise.all([
+      prisma.user.count(),
+      fetch(url.toString(), { method: 'HEAD' }).then((r) => {
+        if (!r.ok) return Promise.reject(r)
+      }),
+    ])
     return new Response('OK')
   } catch (error: unknown) {
     console.log('healthcheck ❌', { error })
