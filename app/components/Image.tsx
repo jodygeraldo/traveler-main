d09f9e79f87ad8f14121b7419d363cc617078120
Jody Geraldo <jody.geraldo.jg@gmail.com>
2022-07-11 15:01:47 +0700

    fix: tooltip not showing on `Image` component

--- app/components/Image.tsx
+++ app/components/Image.tsx
@@ -12,24 +12,23 @@ interface Props
   extends BaseProps,
     Omit<React.ImgHTMLAttributes<HTMLImageElement>, keyof BaseProps> {}
 
-export default function Image({
-  src,
-  alt,
-  className,
-  width,
-  height,
-  ...props
-}: Props) {
-  return (
-    <img
-      src={`//images.weserv.nl/?url=https://traveler-main.fly.dev${src}&w=${width}${
-        height ? '&h=' + height : ''
-      }&output=webp`}
-      alt={alt}
-      className={className}
-      width={width}
-      height={height}
-      {...props}
-    />
-  )
-}
+const Image = React.forwardRef<HTMLImageElement, Props>(
+  ({ src, alt, className, width, height, ...props }, ref) => {
+    return (
+      <img
+        ref={ref}
+        src={`//images.weserv.nl/?url=https://traveler-main.fly.dev${src}&w=${width}${
+          height ? '&h=' + height : ''
+        }&output=webp`}
+        alt={alt}
+        className={className}
+        width={width}
+        height={height}
+        {...props}
+      />
+    )
+  }
+)
+Image.displayName = 'Image'
+
+export default Image
