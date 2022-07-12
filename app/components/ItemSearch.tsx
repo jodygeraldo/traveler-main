a3e5af5cc7f44efd2b727161963dd6cbabeb4b9d
Jody Geraldo <jody.geraldo.jg@gmail.com>
2022-07-12 15:07:40 +0700

    add  component

---
+++ app/components/ItemSearch.tsx
@@ -0,0 +1,35 @@
+import { IconSolid } from './Icon'
+
+interface Props {
+  changeHandler: (e: React.ChangeEvent<HTMLInputElement>) => void
+}
+
+export default function ItemSearch({ changeHandler }: Props) {
+  return (
+    <>
+      <label htmlFor="search" className="sr-only">
+        Search
+      </label>
+      <div className="relative mt-1 rounded-md shadow-sm">
+        <div
+          className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3"
+          aria-hidden="true"
+        >
+          <IconSolid
+            name="search"
+            className="mr-3 h-4 w-4 text-gray-8"
+            aria-hidden="true"
+          />
+        </div>
+        <input
+          type="text"
+          name="search"
+          id="search"
+          onChange={changeHandler}
+          className="block w-full rounded-md border-gray-7 bg-gray-3 pl-9 focus:border-primary-9 focus:ring-primary-9 sm:text-sm focus:outline-none"
+          placeholder="Search"
+        />
+      </div>
+    </>
+  )
+}
