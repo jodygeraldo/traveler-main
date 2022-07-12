ca9ca9236d40f698ba1037e70426ed1d0a23dd64
Jody Geraldo <jody.geraldo.jg@gmail.com>
2022-07-12 17:35:27 +0700

    update landing page

--- app/routes/_index/_index.tsx
+++ app/routes/_index/_index.tsx
@@ -1,131 +1,107 @@
 import * as RemixReact from '@remix-run/react'
+import appCharacterImageUrl from '~/assets/app-character.png'
+import Button, * as Buttons from '~/components/Button'
+import Image from '~/components/Image'
+import Logo from '~/components/Logo'
 import * as Utils from '~/utils'
 
 export default function Index() {
   const user = Utils.useOptionalUser()
 
   return (
-    <main className="relative min-h-screen bg-white sm:flex sm:items-center sm:justify-center">
-      <div className="relative sm:pb-16 sm:pt-8">
-        <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
-          <div className="relative shadow-xl sm:overflow-hidden sm:rounded-2xl">
-            <div className="absolute inset-0">
-              <img
-                className="h-full w-full object-cover"
-                src="https://i.imgur.com/BlcwDoE.jpg"
-                alt="Hyperpop"
-              />
-              <div className="absolute inset-0 bg-[color:rgba(254,204,27,0.5)] mix-blend-multiply" />
-            </div>
-            <div className="lg:pb-18 relative px-4 pt-16 pb-8 sm:px-6 sm:pt-24 sm:pb-14 lg:px-8 lg:pt-32">
-              <h1 className="text-center text-6xl font-extrabold tracking-tight sm:text-8xl lg:text-9xl">
-                <span className="block uppercase italic text-emerald-400 drop-shadow-md">
-                  Hyperpop Stack
-                </span>
-              </h1>
-              <p className="mx-auto mt-6 max-w-lg text-center text-xl text-white sm:max-w-3xl">
-                Check the README.md file for instructions on how to get this
-                project deployed.
-              </p>
-              <div className="mx-auto mt-10 max-w-sm sm:flex sm:max-w-none sm:justify-center">
-                {user ? (
-                  <RemixReact.Link
-                    to="/character"
-                    className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-3 text-base font-medium text-yellow-700 shadow-sm hover:bg-yellow-50 sm:px-8"
-                  >
-                    View Characters for {user.email} on account:{' '}
-                    {user.accounts[0].name}
-                  </RemixReact.Link>
-                ) : (
-                  <div className="space-y-4 sm:mx-auto sm:inline-grid sm:grid-cols-2 sm:gap-5 sm:space-y-0">
-                    <RemixReact.Link
-                      to="/join"
-                      className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-3 text-base font-medium text-yellow-700 shadow-sm hover:bg-yellow-50 sm:px-8"
-                    >
-                      Sign up
-                    </RemixReact.Link>
-                    <RemixReact.Link
-                      to="/login"
-                      className="flex items-center justify-center rounded-md bg-yellow-500 px-4 py-3 font-medium text-white hover:bg-yellow-600  "
-                    >
-                      Log In
-                    </RemixReact.Link>
-                  </div>
-                )}
+    <main className="min-h-screen">
+      <div className="pb-8 sm:pb-12 lg:pb-12">
+        <div className="overflow-hidden pt-8 sm:pt-12 lg:relative lg:py-48">
+          <div className="mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-2 lg:gap-24 lg:px-8">
+            <div>
+              <div>
+                <Logo className="h-11 w-auto" />
               </div>
-              <a href="https://remix.run">
-                <img
-                  src="https://user-images.githubusercontent.com/1500684/158298926-e45dafff-3544-4b69-96d6-d3bcc33fc76a.svg"
-                  alt="Remix"
-                  className="mx-auto mt-16 w-full max-w-[12rem] md:max-w-[16rem]"
-                />
-              </a>
+              <div className="mt-20">
+                <div className="mt-6 sm:max-w-xl">
+                  <h1 className="text-4xl font-extrabold tracking-tight text-gray-12 sm:text-5xl">
+                    Companion For Genshin Impact Nerds
+                  </h1>
+                  <p className="mt-6 text-xl text-gray-11">
+                    helping you to track your characters progression to the max.
+                  </p>
+                </div>
+
+                <div className="mt-6 sm:flex sm:items-center sm:gap-4">
+                  {user ? (
+                    <div>
+                      <RemixReact.Link to="/character" className="block w-full">
+                        <Button className="mt-4 flex w-full justify-center sm:mt-0">
+                          Go to app
+                        </Button>
+                      </RemixReact.Link>
+                    </div>
+                  ) : (
+                    <>
+                      <Buttons.Link
+                        to="/join"
+                        className="flex w-full justify-center"
+                      >
+                        Create new account
+                      </Buttons.Link>
+                      <RemixReact.Link to="/login" className="block w-full">
+                        <Button className="mt-4 flex w-full justify-center sm:mt-0">
+                          Sign in
+                        </Button>
+                      </RemixReact.Link>
+                    </>
+                  )}
+                </div>
+              </div>
             </div>
           </div>
-        </div>
 
-        <div className="mx-auto max-w-7xl py-2 px-4 sm:px-6 lg:px-8">
-          <div className="mt-6 flex flex-wrap justify-center gap-8">
-            {[
-              {
-                src: 'https://user-images.githubusercontent.com/1500684/157764397-ccd8ea10-b8aa-4772-a99b-35de937319e1.svg',
-                alt: 'Fly.io',
-                href: 'https://fly.io',
-              },
-              {
-                src: 'https://www.edgedb.com/logos/blue.svg',
-                alt: 'EdgeDB',
-                href: 'https://www.edgedb.com',
-              },
-              {
-                src: 'https://user-images.githubusercontent.com/1500684/157764276-a516a239-e377-4a20-b44a-0ac7b65c8c14.svg',
-                alt: 'Tailwind',
-                href: 'https://tailwindcss.com',
-              },
-              {
-                src: 'https://user-images.githubusercontent.com/1500684/157764454-48ac8c71-a2a9-4b5e-b19c-edef8b8953d6.svg',
-                alt: 'Cypress',
-                href: 'https://www.cypress.io',
-              },
-              {
-                src: 'https://user-images.githubusercontent.com/1500684/157772386-75444196-0604-4340-af28-53b236faa182.svg',
-                alt: 'MSW',
-                href: 'https://mswjs.io',
-              },
-              {
-                src: 'https://user-images.githubusercontent.com/1500684/157772447-00fccdce-9d12-46a3-8bb4-fac612cdc949.svg',
-                alt: 'Vitest',
-                href: 'https://vitest.dev',
-              },
-              {
-                src: 'https://user-images.githubusercontent.com/1500684/157772662-92b0dd3a-453f-4d18-b8be-9fa6efde52cf.png',
-                alt: 'Testing Library',
-                href: 'https://testing-library.com',
-              },
-              {
-                src: 'https://user-images.githubusercontent.com/1500684/157772934-ce0a943d-e9d0-40f8-97f3-f464c0811643.svg',
-                alt: 'Prettier',
-                href: 'https://prettier.io',
-              },
-              {
-                src: 'https://user-images.githubusercontent.com/1500684/157772990-3968ff7c-b551-4c55-a25c-046a32709a8e.svg',
-                alt: 'ESLint',
-                href: 'https://eslint.org',
-              },
-              {
-                src: 'https://user-images.githubusercontent.com/1500684/157773063-20a0ed64-b9f8-4e0b-9d1e-0b65a3d4a6db.svg',
-                alt: 'TypeScript',
-                href: 'https://typescriptlang.org',
-              },
-            ].map((img) => (
-              <a
-                key={img.href}
-                href={img.href}
-                className="flex h-16 w-32 justify-center p-1 grayscale transition hover:grayscale-0 focus:grayscale-0"
-              >
-                <img alt={img.alt} src={img.src} />
-              </a>
-            ))}
+          <div className="sm:mx-auto sm:max-w-3xl sm:px-6">
+            <div className="py-12 sm:relative sm:mt-12 sm:py-16 lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
+              <div className="hidden sm:block">
+                <div className="absolute inset-y-0 left-1/2 w-screen rounded-l-3xl bg-gray-2 lg:left-80 lg:right-0 lg:w-full" />
+                <svg
+                  className="absolute top-8 right-1/2 -mr-3 lg:left-0 lg:m-0"
+                  width={404}
+                  height={392}
+                  fill="none"
+                  viewBox="0 0 404 392"
+                >
+                  <defs>
+                    <pattern
+                      id="837c3e70-6c3a-44e6-8854-cc48c737b659"
+                      x={0}
+                      y={0}
+                      width={20}
+                      height={20}
+                      patternUnits="userSpaceOnUse"
+                    >
+                      <rect
+                        x={0}
+                        y={0}
+                        width={4}
+                        height={4}
+                        className="text-gray-6"
+                        fill="currentColor"
+                      />
+                    </pattern>
+                  </defs>
+                  <rect
+                    width={404}
+                    height={392}
+                    fill="url(#837c3e70-6c3a-44e6-8854-cc48c737b659)"
+                  />
+                </svg>
+              </div>
+              <div className="relative -mr-40 pl-4 sm:mx-auto sm:max-w-3xl sm:px-0 lg:h-full lg:max-w-none lg:pl-12">
+                <Image
+                  className="w-full rounded-md shadow-xl ring-1 ring-black ring-opacity-5 lg:h-full lg:w-auto lg:max-w-none"
+                  src={appCharacterImageUrl}
+                  width={1280}
+                  alt=""
+                />
+              </div>
+            </div>
           </div>
         </div>
       </div>
