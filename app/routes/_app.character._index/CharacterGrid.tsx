b50e2a43b704e50e51af318d64ac2fccbcae5844
Jody Geraldo <jody.geraldo.jg@gmail.com>
2022-07-15 16:34:57 +0700

    remove radix hover card

---
+++ app/routes/_app.character._index/CharacterGrid.tsx
@@ -0,0 +1,25 @@
+import type * as CharacterData from '~/data/characters'
+import CharacterGridItem from './CharacterGridItem'
+
+interface Props {
+  characters: CharacterData.Character[]
+}
+
+export default function CharacterGrid({ characters }: Props) {
+  return (
+    <div>
+      <ul className="flex flex-wrap gap-5">
+        {characters.map((character) => (
+          <li key={character.name}>
+            <CharacterGridItem
+              name={character.name}
+              vision={character.vision}
+              rarity={character.rarity}
+              level={character.progression?.level}
+            />
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
--- app/routes/_app.character._index/_index.tsx
+++ app/routes/_app.character._index/_index.tsx
@@ -1,8 +1,6 @@
-import * as RadixHoverCard from '@radix-ui/react-hover-card'
 import * as RemixNode from '@remix-run/node'
 import * as RemixReact from '@remix-run/react'
 import clsx from 'clsx'
-import * as React from 'react'
 import * as Zod from 'zod'
 import Button from '~/components/Button'
 import CellWithImage from '~/components/CellWithImage'
@@ -15,6 +13,7 @@ import useSearchFilter from '~/hooks/useSearchFilter'
 import * as CharacterModel from '~/models/character.server'
 import * as Session from '~/session.server'
 import * as Utils from '~/utils'
+import CharacterGrid from './CharacterGrid'
 
 export const meta: RemixNode.MetaFunction = () => ({
   title: 'Characters - Traveler Main',
@@ -152,76 +151,6 @@ export default function CharactersPage() {
   )
 }
 
-const backgroundImage: Record<4 | 5 | '5s', string> = {
-  4: 'bg-image-rarity-4',
-  5: 'bg-image-rarity-5',
-  '5s': 'bg-image-rarity-5s',
-}
-
-function CharacterGrid({
-  characters,
-}: {
-  characters: CharacterData.Character[]
-}) {
-  return (
-    <div>
-      <ul className="flex flex-wrap gap-5">
-        {characters.map((character) => (
-          <li key={character.name}>
-            <HoverCard character={character}>
-              <RemixReact.Link to={`./${character.name}`} prefetch="intent">
-                <div className="group relative rounded-b-md bg-gray-3 shadow-sm hover:bg-gray-4">
-                  <div className="absolute top-0 left-0">
-                    <span className="sr-only">{character.vision} vision</span>
-                    <Image
-                      src={`/element/${character.vision.toLowerCase()}.png`}
-                      alt=""
-                      className="h-6 w-6"
-                      aria-hidden
-                      width={24}
-                      height={24}
-                    />
-                  </div>
-
-                  <div
-                    className={clsx(
-                      backgroundImage[
-                        character.name === 'Aloy' ? '5s' : character.rarity
-                      ],
-                      'rounded-t-md rounded-br-3xl'
-                    )}
-                  >
-                    <Image
-                      src={`/character/${Utils.getImageSrc(
-                        character.name
-                      )}.png`}
-                      alt={character.name}
-                      className="h-24 w-24 rounded-br-3xl"
-                      width={96}
-                      height={96}
-                    />
-                  </div>
-                  <div className="mt-1 text-center">
-                    <span className="sr-only">
-                      Level {character.progression?.level ?? 1}
-                    </span>
-                    <p
-                      className="text-sm text-gray-11 group-hover:text-gray-12"
-                      aria-hidden
-                    >
-                      Lv. {character.progression?.level ?? 1}
-                    </p>
-                  </div>
-                </div>
-              </RemixReact.Link>
-            </HoverCard>
-          </li>
-        ))}
-      </ul>
-    </div>
-  )
-}
-
 function CharacterList({
   characters,
 }: {
@@ -235,70 +164,6 @@ function CharacterList({
         ))}
       </ul>
     </div>
-  )
-}
-
-function HoverCard({
-  character,
-  children,
-}: {
-  character: CharacterData.Character
-  children: React.ReactNode
-}) {
-  return (
-    <RadixHoverCard.Root openDelay={300}>
-      <RadixHoverCard.Trigger asChild>{children}</RadixHoverCard.Trigger>
-      <RadixHoverCard.Content
-        side="top"
-        className="motion-safe:slideAndFade rounded-md bg-gray-3 p-4 shadow-lg"
-      >
-        <h2 className="text-lg font-medium leading-6 text-primary-12">
-          {character.name}
-        </h2>
-        <div className="flex items-center gap-4">
-          <div className="mt-1 w-full text-gray-11">
-            <p>Ascension {character.progression?.ascension ?? 0}</p>
-            <div className="flex w-full items-center gap-3">
-              <span className="inline-flex items-center gap-0.5">
-                <Image
-                  src={`/talent/normal_attack_${character.weapon.toLowerCase()}.png`}
-                  alt={character.talent[0]}
-                  className="h-5 w-5 flex-shrink-0"
-                  width={20}
-                  height={20}
-                />
-                <span>{character.progression?.normalAttack ?? 1}</span>
-              </span>
-              <span className="inline-flex items-center gap-0.5">
-                <Image
-                  src={`/talent/elemental_skill_${Utils.getImageSrc(
-                    character.name
-                  )}.png`}
-                  alt={character.talent[1]}
-                  className="h-5 w-5 flex-shrink-0"
-                  width={20}
-                  height={20}
-                />
-                <span>{character.progression?.elementalSkill ?? 1}</span>
-              </span>
-              <span className="inline-flex items-center gap-0.5">
-                <Image
-                  src={`/talent/elemental_burst_${Utils.getImageSrc(
-                    character.name
-                  )}.png`}
-                  alt={character.talent[2]}
-                  className="h-5 w-5 flex-shrink-0"
-                  width={20}
-                  height={20}
-                />
-                <span>{character.progression?.elementalBurst ?? 1}</span>
-              </span>
-            </div>
-          </div>
-        </div>
-        <RadixHoverCard.Arrow className="fill-gray-3" />
-      </RadixHoverCard.Content>
-    </RadixHoverCard.Root>
   )
 }
 
