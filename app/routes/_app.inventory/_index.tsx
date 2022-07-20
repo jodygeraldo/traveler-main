99bcd38c98bdb1787c46eeeef0633172ce14a0c1
Jody Geraldo <jody.geraldo.jg@gmail.com>
2022-07-21 06:18:55 +0700

    fix: duplicate sidebar label

--- app/routes/_app.inventory/_index.tsx
+++ app/routes/_app.inventory/_index.tsx
@@ -44,9 +44,9 @@ export default function InventoryLayout() {
     <div className="py-10">
       <div className="mx-auto max-w-3xl px-4 sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-12 lg:gap-8 lg:px-8">
         <div className="col-span-12 lg:col-span-2">
-          <nav aria-label="Sidebar" className="sticky top-6">
+          <div className="sticky top-6">
             <Sidebar navigation={navigation} />
-          </nav>
+          </div>
         </div>
         <main className="mt-8 lg:col-span-10 lg:mt-0">
           <RemixReact.Outlet />
