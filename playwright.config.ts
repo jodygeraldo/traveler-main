facc634b64f47991909a7c41aca4d6b67c0ef101
Jody Geraldo <ajody.geraldo@gmail.com>
2022-07-22 14:46:14 +0700

    update playwright timeout

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,7 +5,7 @@ dotenv.config()
 
 const config: PlaywrightTestConfig = {
   testDir: './e2e',
-  timeout: 15 * 60 * 1000,
+  timeout: 15 * 1000,
   globalTimeout: process.env.CI ? 60 * 60 * 1000 : undefined,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
