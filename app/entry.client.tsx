83830aa7d2c7a35734502eeaf04c59857ef31a92
Jody Geraldo <jody.geraldo.jg@gmail.com>
2022-07-24 11:36:42 +0700

    react on strict mode

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,4 +1,10 @@
 import * as RemixReact from '@remix-run/react'
+import * as React from 'react'
 import * as ReactDOMClient from 'react-dom/client'
 
-ReactDOMClient.hydrateRoot(document, <RemixReact.RemixBrowser />)
+ReactDOMClient.hydrateRoot(
+  document,
+  <React.StrictMode>
+    <RemixReact.RemixBrowser />
+  </React.StrictMode>
+)
