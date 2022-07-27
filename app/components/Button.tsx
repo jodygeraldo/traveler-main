7b354f7e696f6fcb6015c59dabb6a984eacab415
Jody Geraldo <ajody.geraldo@gmail.com>
2022-07-27 16:29:55 +0700

    remove `:enable` modifier as it make hover effect not work on link

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -83,18 +83,18 @@ const baseStyles: Record<KindProps, string> = {
 
 const variantStyles: Record<VariantProps, string> = {
   primary:
-    'bg-primary-9 text-white hover:enabled:bg-primary-10 focus:ring-primary-8 disabled:bg-primary-6 disabled:text-primary-11',
+    'bg-primary-9 text-white hover:bg-primary-10 focus:ring-primary-8 disabled:bg-primary-6 disabled:text-primary-11',
   secondary:
-    'bg-primary-3 text-primary-11 hover:enabled:bg-primary-4 focus:ring-primary-8 disabled:bg-primary-6',
+    'bg-primary-3 text-primary-11 hover:bg-primary-4 focus:ring-primary-8 disabled:bg-primary-6',
   basic:
-    'bg-gray-3 text-gray-11 hover:enabled:bg-gray-4 focus:ring-gray-8 disabled:bg-gray-6',
+    'bg-gray-3 text-gray-11 hover:bg-gray-4 focus:ring-gray-8 disabled:bg-gray-6',
   success:
-    'bg-success-3 text-success-11 hover:enabled:bg-success-4 focus:ring-success-8 disabled:bg-success-6',
+    'bg-success-3 text-success-11 hover:bg-success-4 focus:ring-success-8 disabled:bg-success-6',
   warning:
-    'bg-warning-3 text-warning-11 hover:enabled:bg-warning-4 focus:ring-warning-8 disabled:bg-warning-6',
-  info: 'bg-info-3 text-info-11 hover:enabled:bg-info-4 focus:ring-info-8 disabled:bg-info-6',
+    'bg-warning-3 text-warning-11 hover:bg-warning-4 focus:ring-warning-8 disabled:bg-warning-6',
+  info: 'bg-info-3 text-info-11 hover:bg-info-4 focus:ring-info-8 disabled:bg-info-6',
   danger:
-    'bg-danger-3 text-danger-11 hover:enabled:bg-danger-4 focus:ring-danger-8 disabled:bg-danger-6',
+    'bg-danger-3 text-danger-11 hover:bg-danger-4 focus:ring-danger-8 disabled:bg-danger-6',
 }
 
 const focusRingStyles: Record<FocusRingProps, string> = {
