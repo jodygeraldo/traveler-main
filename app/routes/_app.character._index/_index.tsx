7d0941ecab8498db5ca523a28127fa84888a8706
Jody Geraldo <ajody.geraldo@gmail.com>
2022-08-01 16:31:56 +0700

    remove link to `quick-update page` until better implementation

--- app/routes/_app.character._index/_index.tsx
+++ app/routes/_app.character._index/_index.tsx
@@ -2,7 +2,6 @@ import * as RemixNode from '@remix-run/node'
 import * as RemixReact from '@remix-run/react'
 import clsx from 'clsx'
 import * as Zod from 'zod'
-import * as Button from '~/components/Button'
 import * as Icon from '~/components/Icon'
 import Search from '~/components/Search'
 import * as Cookie from '~/cookies'
@@ -58,75 +57,84 @@ export default function CharactersPage() {
 
   return (
     <main className="mx-auto max-w-3xl py-10 px-4 sm:px-6 lg:max-w-7xl lg:px-8">
-      <div className="flex items-center">
-        <h1 className="text-2xl font-bold leading-7 text-primary-12 sm:truncate sm:text-3xl">
-          Characters
-        </h1>
+      <div className="sm:flex sm:items-center sm:justify-between">
+        <div className="flex items-center">
+          <h1 className="text-2xl font-bold leading-7 text-primary-12 sm:truncate sm:text-3xl">
+            Characters
+          </h1>
 
-        <fetcher.Form
-          action="/character?index"
-          method="post"
-          replace={true}
-          className="ml-6 flex items-center rounded-lg bg-gray-3 p-0.5"
-        >
-          <button
-            id="switch-list-view"
-            type="submit"
-            name="characterView"
-            value="list"
-            className={clsx(
-              optimisticView === 'list'
-                ? 'bg-gray-2 shadow-sm'
-                : 'hover:bg-gray-2 hover:shadow-sm',
-              'rounded-md p-1.5 text-gray-10 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-primary-8'
-            )}
+          <fetcher.Form
+            action="/character?index"
+            method="post"
+            replace={true}
+            className="ml-6 flex items-center rounded-lg bg-gray-3 p-0.5"
           >
-            {optimisticView === 'list' ? (
-              <Icon.Solid
-                name="viewList"
-                className="h-5 w-5"
-                aria-hidden="true"
-              />
-            ) : (
-              <Icon.Outline
-                name="viewList"
-                className="h-5 w-5"
-                aria-hidden="true"
-              />
-            )}
-            <span className="sr-only">Use list view</span>
-          </button>
-          <button
-            id="switch-grid-view"
-            type="submit"
-            name="characterView"
-            value="grid"
-            className={clsx(
-              optimisticView === 'list'
-                ? 'hover:bg-gray-2 hover:shadow-sm'
-                : 'bg-gray-2 shadow-sm',
-              'ml-0.5 rounded-md p-1.5 text-gray-10 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-primary-8'
-            )}
-          >
-            {optimisticView === 'list' ? (
-              <Icon.Outline
-                name="viewGrid"
-                className="h-5 w-5"
-                aria-hidden="true"
-              />
-            ) : (
-              <Icon.Solid
-                name="viewGrid"
-                className="h-5 w-5"
-                aria-hidden="true"
-              />
-            )}
-            <span className="sr-only">Use grid view</span>
-          </button>
-        </fetcher.Form>
+            <button
+              id="switch-list-view"
+              type="submit"
+              name="characterView"
+              value="list"
+              className={clsx(
+                optimisticView === 'list'
+                  ? 'bg-gray-2 shadow-sm'
+                  : 'hover:bg-gray-2 hover:shadow-sm',
+                'rounded-md p-1.5 text-gray-10 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-primary-8'
+              )}
+            >
+              {optimisticView === 'list' ? (
+                <Icon.Solid
+                  name="viewList"
+                  className="h-5 w-5"
+                  aria-hidden="true"
+                />
+              ) : (
+                <Icon.Outline
+                  name="viewList"
+                  className="h-5 w-5"
+                  aria-hidden="true"
+                />
+              )}
+              <span className="sr-only">Use list view</span>
+            </button>
+            <button
+              id="switch-grid-view"
+              type="submit"
+              name="characterView"
+              value="grid"
+              className={clsx(
+                optimisticView === 'list'
+                  ? 'hover:bg-gray-2 hover:shadow-sm'
+                  : 'bg-gray-2 shadow-sm',
+                'ml-0.5 rounded-md p-1.5 text-gray-10 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-primary-8'
+              )}
+            >
+              {optimisticView === 'list' ? (
+                <Icon.Outline
+                  name="viewGrid"
+                  className="h-5 w-5"
+                  aria-hidden="true"
+                />
+              ) : (
+                <Icon.Solid
+                  name="viewGrid"
+                  className="h-5 w-5"
+                  aria-hidden="true"
+                />
+              )}
+              <span className="sr-only">Use grid view</span>
+            </button>
+          </fetcher.Form>
+        </div>
+
+        <div className="mt-4 sm:mt-0">
+          <Search
+            changeHandler={changeHandler}
+            placeholder="Search character"
+          />
+        </div>
       </div>
 
-      <div className="mt-4 flex items-center gap-4">
+      {/* <div className="mt-4 flex items-center gap-4">
         <Button.Link
           styles="button"
           prefetch="intent"
@@ -136,10 +144,9 @@ export default function CharactersPage() {
           Quick update
         </Button.Link>
 
-        <Search changeHandler={changeHandler} placeholder="Search character" />
-      </div>
+      </div> */}
 
-      <div className="mt-8">
+      <div className="mt-12">
         {optimisticView === 'list' ? (
           <CharacterList characters={showSearch ? searchItems : characters} />
         ) : (
