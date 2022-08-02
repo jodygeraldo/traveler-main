5d533963ef76553bcdbaf1bf57072be6d714c525
Jody Geraldo <ajody.geraldo@gmail.com>
2022-08-02 22:12:19 +0700

    reuse auth session
    completed track page flow
    broke others test

--- e2e/app.spec.ts
+++ e2e/app.spec.ts
@@ -1,40 +1,47 @@
-import { expect, test } from '@playwright/test'
 import prisma from '~/db.server'
+import { expect, test, users } from './fixtures'
 
 const OK_STATUS_TEXT = 'SUCCESS'
+const BASE_URL = 'http://localhost:3000'
+
+async function cleanupUser({
+  email,
+  accoundId,
+}: {
+  email: string
+  accoundId: string
+}) {
+  await prisma.user
+    .update({
+      where: { email },
+      data: {
+        accounts: {
+          update: {
+            where: { id: accoundId },
+            data: {
+              inventory: { deleteMany: {} },
+              characters: { deleteMany: {} },
+            },
+          },
+        },
+      },
+    })
+    .catch((e) => console.error(e))
+}
 
 test.describe('apps', () => {
-  const PASSWORD = 'test1234'
-
-  test.beforeEach(async ({ page }, testInfo) => {
-    const EMAIL = `test${testInfo.line}@test.com`
-
-    await prisma.user
-      .delete({
-        where: { email: EMAIL },
-      })
-      .catch(() => {})
-
-    await page.goto('.')
-    await page.click('#signup')
-    await page.fill('#email', EMAIL)
-    await page.fill('#password', PASSWORD)
-    await page.fill('#confirm-password', PASSWORD)
-    await Promise.all([page.waitForNavigation(), page.click('#signup')])
-    await expect(page).toHaveURL('/character')
+  test.beforeEach(async ({ page: _ }, testInfo) => {
+    const currentUser = users[testInfo.workerIndex]
+    await cleanupUser(currentUser)
   })
-
-  // cleanup user
   test.afterEach(async ({ page: _ }, testInfo) => {
-    await prisma.user
-      .delete({
-        where: { email: `test${testInfo.line}@test.com` },
-      })
-      .catch(() => {})
+    const currentUser = users[testInfo.workerIndex]
+    await cleanupUser(currentUser)
   })
 
   test.describe('character page', () => {
     test('all characters should be displayed', async ({ page }) => {
+      await page.goto('/character')
       const characterLen = await prisma.character.count()
       await expect(page.locator('#grid-view > li')).toHaveCount(characterLen)
     })
@@ -131,7 +138,7 @@ test.describe('apps', () => {
 
   test.describe('inventory page', () => {
     test('can go to inventory page', async ({ page }) => {
-      await page.locator('#Inventory-link-desktop').click()
+      await page.click('#Inventory-link-desktop')
       await expect(page).toHaveURL('/inventory/all')
       await expect(page.locator('h1:has-text("Inventory")')).toBeVisible()
     })
@@ -159,7 +166,6 @@ test.describe('apps', () => {
 
       await page.click('#all-link')
       await expect(page).toHaveURL('/inventory/all')
-      await page.locator(ITEM[0].SELECTOR).fill('120')
     })
 
     test('can search for items', async ({ page }) => {
@@ -169,7 +175,7 @@ test.describe('apps', () => {
         page.waitForResponse(
           async (response) => response.statusText() === OK_STATUS_TEXT
         ),
-        await page.locator(ITEM[2].SELECTOR).fill(ITEM[2].QUANTITY),
+        await page.fill(ITEM[2].SELECTOR, ITEM[2].QUANTITY),
       ])
 
       await page.fill('#search', 'dream sol')
@@ -443,7 +449,7 @@ test.describe('apps', () => {
         page.click('#jump-level'),
       ])
 
-      await page.locator('#character-level').fill('30')
+      await page.fill('#character-level', '30')
       await Promise.all([
         page.waitForResponse(
           async (response) => response.statusText() === OK_STATUS_TEXT
@@ -462,24 +468,104 @@ test.describe('apps', () => {
     })
   })
 
-  test.describe('track page', () => {
-    test('Should display empty state when no track', async ({ page }) => {
-      await page.locator('#Track-link-desktop').click()
-      await page.waitForURL('/track')
-      await expect(page.locator('h1:has-text("Tracks")')).toBeVisible()
-      await expect(
-        page.locator('a:has-text("Track a character")')
-      ).toBeVisible()
-    })
+  test.only('Track page flow', async ({ page }) => {
+    await page.goto('/character')
+    await page.click('#Track-link-desktop')
+    await expect(page).toHaveURL(`${BASE_URL}/track`)
+    await expect(page.locator('h1:has-text("Tracks")')).toBeVisible()
 
-    test('Should able to add track', async ({ page }) => {
-      await page.goto('/track')
-      await Promise.all([
-        page.waitForNavigation(),
-        page.click('a:has-text("Track a character")'),
-      ])
+    await page.locator('a:has-text("Track a character")').click()
+    await expect(page).toHaveURL(`${BASE_URL}/track/add`)
 
-      expect(page.url()).toMatch('/track/add')
-    })
+    await page.locator('input[role="combobox"]').click()
+    await page.locator('input[role="combobox"]').fill('diluc')
+    await page.locator('ul[role="listbox"] div:has-text("Diluc")').click()
+    await page.locator('text=Update progression').click()
+    await expect(
+      page.locator('text=Last updated progression for Diluc')
+    ).toBeVisible()
+
+    await page.locator('input[name="level"]').click()
+    await page.locator('input[name="level"]').fill('40')
+
+    await page.locator('input[name="ascension"]').click()
+    await page.locator('input[name="ascension"]').fill('2')
+
+    await page.locator('input[name="normalAttack"]').click()
+    await page.locator('input[name="normalAttack"]').fill('6')
+
+    await page.locator('input[name="normalAttack"]').press('Tab')
+    await page.locator('input[name="elementalSkill"]').fill('6')
+
+    await page.locator('input[name="elementalSkill"]').press('Tab')
+    await page.locator('input[name="elementalBurst"]').fill('6')
+
+    await Promise.all([
+      page.waitForResponse((response) => response.status() === 400),
+      page.locator('button:has-text("Track")').click(),
+    ])
+
+    await expect(
+      page.locator('text=Maximum normal attack on ascension 2 is 2')
+    ).toBeVisible()
+    await expect(
+      page.locator('text=Maximum elemental skill on ascension 2 is 2')
+    ).toBeVisible()
+    await expect(
+      page.locator('text=Maximum elemental burst on ascension 2 is 2')
+    ).toBeVisible()
+
+    await page.locator('input[name="normalAttack"]').click()
+    await page.locator('input[name="normalAttack"]').fill('2')
+
+    await page.locator('input[name="normalAttack"]').click()
+    await page.locator('input[name="elementalSkill"]').fill('2')
+
+    await page.locator('input[name="elementalSkill"]').click()
+    await page.locator('input[name="elementalBurst"]').fill('2')
+
+    await page.locator('button:has-text("Track")').click()
+    await expect(page).toHaveURL(`${BASE_URL}/track`)
+
+    await expect(page.locator('text=Diluc')).toBeVisible()
+    await expect(page.locator('text=Level 1')).toBeVisible()
+    await expect(page.locator('text=Ascension 0')).toBeVisible()
+    await expect(page.locator('text=Normal Attack 1')).toBeVisible()
+    await expect(page.locator('text=Elemental Skill 1')).toBeVisible()
+    await expect(page.locator('text=Elemental Burst 1')).toBeVisible()
+
+    await page.locator('a[href="/track/Diluc"]').click()
+    await expect(page).toHaveURL(`${BASE_URL}/track/Diluc`)
+
+    await expect(page.locator('h4:has-text("Ascension")')).toBeVisible()
+    await page.locator('text=Increase level').click()
+    await page.locator('text=Decrease level').click()
+    await page.locator('text=Increase level').click()
+    await page.locator('text=Increase level').click()
+    await expect(page.locator('text=Increase level')).toHaveCount(3)
+
+    await page.locator('text=Increase level').first().click()
+    await expect(page.locator('h4:has-text("Normal Attack")')).toBeHidden()
+
+    await page.locator('text=Increase level').nth(1).click()
+
+    await page.locator('a:has-text("Track")').click()
+    await expect(page).toHaveURL(`${BASE_URL}/track`)
+
+    await page.locator('a[href="/track/update/Diluc"]').click()
+    await expect(page).toHaveURL(`${BASE_URL}/track/update/Diluc`)
+
+    await page.locator('input[name="ascension"]').click()
+    await page.locator('input[name="ascension"]').fill('3')
+
+    await page.locator('button:has-text("Update")').click()
+    await expect(page).toHaveURL(`${BASE_URL}/track`)
+
+    await expect(page.locator('text=Ascension 2')).toBeVisible()
+    await expect(page.locator('text=Elemental Skill 1')).toBeVisible()
+
+    await page.locator('text=delete').click()
+    await expect(page).toHaveURL(`${BASE_URL}/track`)
+    await expect(page.locator('a[href="/track/Diluc"]')).toBeHidden()
   })
 })
---
+++ e2e/fixtures.ts
@@ -0,0 +1,58 @@
+import { expect, test as baseTest } from '@playwright/test'
+import fs from 'node:fs'
+import path from 'node:path'
+export { expect }
+
+export const users = [
+  {
+    email: 'playwright1@jodygeraldo.com',
+    password: 'playwright1234',
+    accoundId: 'cl6c6wyqy2254n4veqz2bcxcf',
+  },
+  {
+    email: 'playwright2@jodygeraldo.com',
+    password: 'playwright1234',
+    accoundId: 'cl6c6xbdk2279n4veykbfb3v4',
+  },
+  {
+    email: 'playwright3@jodygeraldo.com',
+    password: 'playwright1234',
+    accoundId: 'cl6c6xmn62304n4ve8gne1xvo',
+  },
+  {
+    email: 'playwright4@jodygeraldo.com',
+    password: 'playwright1234',
+    accoundId: 'cl6c6xzno2329n4vepct8on7a',
+  },
+  {
+    email: 'playwright@jodygeraldo.com',
+    password: 'playwright1234',
+    accoundId: 'cl6c5vo4p2153n4ve7m76q3fd',
+  },
+]
+
+export const test = baseTest.extend({
+  storageState: async ({ browser }, use, testInfo) => {
+    // Override storage state, use worker index to look up logged-in info and generate it lazily.
+    const fileName = path.join(
+      testInfo.project.outputDir,
+      'storage-' + testInfo.workerIndex + '.json'
+    )
+    if (!fs.existsSync(fileName)) {
+      // Make sure we are not using any other storage state.
+      const page = await browser.newPage({ storageState: undefined })
+      await page.goto('http://localhost:3000/login')
+      // Create a unique username for each worker.
+      await page.locator('#email').fill(users[testInfo.workerIndex].email)
+      await page.locator('#password').fill(users[testInfo.workerIndex].password)
+      await Promise.all([
+        page.waitForResponse((response) => response.status() === 200),
+        page.click('#signin'),
+      ])
+      await expect(page).toHaveURL('http://localhost:3000/character')
+      await page.context().storageState({ path: fileName })
+      await page.close()
+    }
+    await use(fileName)
+  },
+})
