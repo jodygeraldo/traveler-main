8a61cb843177488202997a5a2b41b68b617c4124
Jody Geraldo <ajody.geraldo@gmail.com>
2022-08-03 17:46:31 +0700

    only retry once

--- playwright.config.ts
+++ playwright.config.ts
@@ -7,7 +7,7 @@ const config: PlaywrightTestConfig = {
   testDir: './e2e',
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 1 : 0,
   reporter: process.env.CI ? 'github' : 'list',
 
   workers: 4,
