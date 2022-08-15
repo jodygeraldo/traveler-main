e5493f3ad13593a91c08a028b81b8cbc7ed906db
Jody Geraldo <jody.geraldo.jg@gmail.com>
2022-08-15 17:09:33 +0700

    add stats component

--- app/utils/server/handbook.server.ts
+++ app/utils/server/handbook.server.ts
@@ -2,6 +2,7 @@ import * as DateFns from 'date-fns'
 import * as DateFnsTz from 'date-fns-tz'
 import * as FarmableData from '~/data/farmable.server'
 import type * as DB from '~/db.server'
+import type * as CharacterType from '~/types/character'
 
 export function getGenshinDailyReset(server: DB.Server) {
   const jakartaTime = DateFnsTz.zonedTimeToUtc(new Date(), 'Asia/Jakarta')
@@ -49,4 +50,49 @@ export function getFarmable(server: DB.Server) {
       : DateFns.addDays(jakartaTime, -1).getDay()
 
   return FarmableData.farmable[currentDay]
+}
+
+export function getStats(
+  characters: ({ name: string } & CharacterType.Progression)[]
+) {
+  return [
+    {
+      name: 'Characters Ascension 6',
+      value: Math.round(
+        characters.reduce((acc, cur) => {
+          if (cur.name.includes('Traveler') && cur.ascension === 6)
+            return (
+              acc +
+              1 / characters.filter((c) => c.name.includes('Traveler')).length
+            )
+          if (cur.ascension === 6) return acc + 1
+          return acc
+        }, 0)
+      ),
+    },
+    {
+      name: 'Characters Level 90',
+      value: Math.round(
+        characters.reduce((acc, cur) => {
+          if (cur.name.includes('Traveler') && cur.level === 90)
+            return (
+              acc +
+              1 / characters.filter((c) => c.name.includes('Traveler')).length
+            )
+          if (cur.level === 90) return acc + 1
+          return acc
+        }, 0)
+      ),
+    },
+    {
+      name: 'Talents Level 8+',
+      value: characters.reduce((acc, cur) => {
+        let count = 0
+        cur.normalAttack >= 8 && (count += 1)
+        cur.elementalSkill >= 8 && (count += 1)
+        cur.elementalBurst >= 8 && (count += 1)
+        return acc + count
+      }, 0),
+    },
+  ]
 }
