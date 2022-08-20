3e9a30c9f299381e47f8df1380f7c0088b2a0908
Jody Geraldo <jody.geraldo.jg@gmail.com>
2022-08-20 18:03:07 +0700

    add min height for page heading

--- app/components/PageHeading.tsx
+++ app/components/PageHeading.tsx
@@ -7,7 +7,7 @@ type Props = {
 
 export default function PageHeading({ title, children }: Props) {
   return (
-    <div className="border-b border-gray-6 px-4 py-4 sm:flex sm:items-center sm:justify-between sm:px-6 lg:bg-gray-2 lg:px-8">
+    <div className="min-h-[4.5rem] border-b border-gray-6 px-4 py-4 sm:flex sm:items-center sm:justify-between sm:px-6 lg:bg-gray-2 lg:px-8">
       <div className="min-w-0 flex-1">
         <h1 className="text-lg font-medium leading-6 text-gray-12 sm:truncate">
           {title}
